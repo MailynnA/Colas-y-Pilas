4f3baddd496e7a448a9c43e58bd9907a5d6927f0
Mailyn <mayinandrea28@gmail.com>
2025-04-03 08:32:12 -0500

    Pilas y Colas

---
+++ Colas.php
@@ -0,0 +1,20 @@
+<?php
+
+$cola = array();
+
+array_push($cola, "cliente 1");
+array_push($cola, "cliente 2");
+array_push($cola, "cliente 3");
+array_push($cola, "cliente 4");
+array_push($cola, "cliente 5");
+array_push($cola, "cliente 6");
+array_push($cola, "cliente 7");
+
+
+
+$cliente = array_shift($cola);
+
+echo "El primer cliente que llego a esta cola fue: ".$cliente
+
+
+?>
